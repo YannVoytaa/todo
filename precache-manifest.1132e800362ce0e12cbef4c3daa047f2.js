9deba7ce84e873c5604b61aa91c164559b1640eb
YannVoytaa <wojtachjanek@gmail.com>
2021-02-22 15:02:27 +0100

    Updates

---
+++ precache-manifest.1132e800362ce0e12cbef4c3daa047f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a5459fcd34903fe1b1dcd381fb06de",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "72e16259b23baae2cf99",
+    "url": "/todo/static/css/main.c6f8227c.chunk.css"
+  },
+  {
+    "revision": "6fc2fa10e39f984e82bf",
+    "url": "/todo/static/js/2.459182be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.459182be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72e16259b23baae2cf99",
+    "url": "/todo/static/js/main.2e29a66b.chunk.js"
+  },
+  {
+    "revision": "9378a18a2228b59e64d2",
+    "url": "/todo/static/js/runtime-main.4ac90e0f.js"
+  }
+]);
